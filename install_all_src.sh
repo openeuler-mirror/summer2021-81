74e4eb46085e33e84cecb4a831824f4555a8f5b7
Wang Haitao <wanghaitao.ouyang@foxmail.com>
2021-08-26 10:50:26 +0800

    update scripts

--- install_all_src.sh
+++ install_all_src.sh
@@ -1,1 +1,11 @@
+if [[ -z "$(grep -w mockbuild /etc/passwd)" ]];then
+    printf create user mockbuild, please input root passwd
+    sudo useradd mockbuild
+fi
+
+if [[ -z "$(grep -w mock /etc/group)" ]];then
+    printf create group mock, please input root passwd
+    sudo groupadd mock
+fi
+
 find . -name "*src.rpm" | xargs rpm -i
